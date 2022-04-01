a9601dc4a726c7319bb25cae5dc75594a8fa66bf
Kakhorov Aziz <kaxopob.azizuz@gmail.com>
2022-04-01 16:18:49 +0500

    add #give_card

--- game.rb
+++ game.rb
@@ -1,36 +1,47 @@
-class Game
-  CARDS = {"2^": 2, "2+": 2, "2<3": 2, "2<>": 2, "2^": 2,
-           "3^": 3, "3+": 3, "3<3": 3, "3<>": 3, "3^": 3,
-           "4^": 4, "4+": 4, "4<3": 4, "4<>": 4, "4^": 4,
-           "5^": 5, "5+": 5, "5<3": 5, "5<>": 5, "5^": 5,
-           "6^": 6, "6+": 6, "6<3": 6, "6<>": 6, "6^": 6,
-           "7^": 7, "7+": 7, "7<3": 7, "7<>": 7, "7^": 7,
-           "8^": 8, "8+": 8, "8<3": 8, "8<>": 8, "8^": 8,
-           "9^": 9, "9+": 9, "9<3": 9, "9<>": 9, "9^": 9,
-           "10^": 10, "10+": 10, "10<3": 10, "10<>": 10, "10^": 10,
-           "J^": 10, "J+": 10, "J<3": 10, "J<>": 10, "J^": 10,
-           "Q^": 10, "Q+": 10, "Q<3": 10, "Q<>": 10, "Q^": 10,
-           "K^": 10, "K+": 10, "K<3": 10, "K<>": 10, "K^": 10,
-           "T^": [1, 10], "T+": [1, 10], "T<3": [1, 10], "T<>": [1, 10], "T^": [1, 10],
-          }
+require 'pry'
 
+class Game
+  CARDS = {"2^": 2, "2+": 2, "2<3": 2, "2<>": 2,
+           "3^": 3, "3+": 3, "3<3": 3, "3<>": 3,
+           "4^": 4, "4+": 4, "4<3": 4, "4<>": 4,
+           "5^": 5, "5+": 5, "5<3": 5, "5<>": 5,
+           "6^": 6, "6+": 6, "6<3": 6, "6<>": 6,
+           "7^": 7, "7+": 7, "7<3": 7, "7<>": 7,
+           "8^": 8, "8+": 8, "8<3": 8, "8<>": 8,
+           "9^": 9, "9+": 9, "9<3": 9, "9<>": 9,
+           "10^": 10, "10+": 10, "10<3": 10, "10<>": 10,
+           "J^": 10, "J+": 10, "J<3": 10, "J<>": 10,
+           "Q^": 10, "Q+": 10, "Q<3": 10, "Q<>": 10,
+           "K^": 10, "K+": 10, "K<3": 10, "K<>": 10,
+           "A^": [1, 10], "A+": [1, 10], "A<3": [1, 10], "A<>": [1, 10]
+          }
+  attr_accessor :player, :total_cash, :dealer_cash, :cards_in_game,
+                        :dealer_cards, :dealer_points, :dealer_bid
 
   def initialize
     @total_cash = 0
     @dealer_cash = 100
     @cards_in_game = CARDS
-    @dealer_cards_now = []
+    @dealer_cards = []
     @dealer_points = 0
     @dealer_bid = 10
     @player = nil
   end
 
   def begigning_game
+    puts "The game has started!"
 
+  end
+
+  def give_card
+    card_num = rand(cards_in_game.size)
+    cards = cards_in_game.to_a
+    card = cards[card_num - 1]
+    self.cards_in_game.delete(card.first)
+    Hash[card.first, card.last]
   end
 
   private
 
-  attr_accessor :player, :total_cash, :dealer_cash, :cards_in_game,
-                :dealer_cards_now, :dealer_points, :dealer_bid
+
 end
--- player.rb
+++ player.rb
@@ -4,13 +4,13 @@ class Player
 
   def initialize(name)
     @name, @player_cash = name, STARTMONEY
-    @player_cards_now = []
+    @player_cards = []
     @player_points = 0
     @player_bid = 10
   end
 
   private
 
-  attr_accessor :player_cash, :player_bid, :player_cards_now, :player_points
+  attr_accessor :player_cash, :player_bid, :player_cards, :player_points
   attr_reader :name
 end
